3c4fea1c3934674696162b9fdbfb9c0b0aeec030
Rafael Amador Galvan <rafael.amador@gmail.com>
2018-10-03 10:21:03 -0500

    MAking changes to the utility to make more clear the redaction and help ouput in the file ... changed token in output file to make more clear in which files the differences are detected.

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ An utility to quick compare shasum output files from directories
 
 ## Dependencies
 
-	* python 3
-		* argparse library
+ * python 3
+ * argparse library
 
 ### Generating SHASUM files
 
-First you need to create 2 shasum files with different directories, if you don't know how. you can do it with:
+First you need to create 2 shasum files with different directories, if you don't know how, you can do it with this bash script:
 
 ```Bash
 find {directory} -type f -exec shasum -a 256 {} \; > {output_file}
@@ -18,15 +18,29 @@ This command will generate a file using the algorithm SHA256 to compute recursiv
 
 ### How to use this tool 
 
+before you can use this tool you need to modify two variables in `checksum.py` named `token1` and `token2` with the directory name used in each file to compute the hashes
+
 ```Bash
 python3 checksum.py --prod={file1} --resp={file2} --out={output_file}
 ```
 
-Parameters
+Running this command will generate an output file comparing both files named resultados.txt, opening this file will give information about which files are diferent or missing in one file or another.
 
 ```
-	--prod : Specify the first SHASUM filename to compare
-	--resp : Specify the second SHASUM filename to compare
+[-1-] = the file is only present in file1
+[-2-] = the file is only present in file2
+[=/=] = the file is present in both files but the content in both files are different.
+```
+### Parameters
+
+```
+	--file1 : Specify the first SHASUM filename to compare
+	--file2 : Specify the second SHASUM filename to compare
 	--out  : Specify a custom output filename
 ```
 
+### TO-DO
+
+ * Remove the requeriment to edit `token1` and `token2` variables.
+ * Transate all the commentaries in Spanich to English.
+ * Improve the speed of the tool.
--- checksum.py
+++ checksum.py
@@ -12,8 +12,8 @@ import argparse
 # la carpeta respaldo
 # para generar estos archivos con estructura adecuada puedes ejecutar
 # find [directory] -type f -exec shasum -a 256 {} \; > [output_file]
-token_prod = "mibs_1"
-token_resp = "mibs_2"
+token1 = "mibs_1"
+token2 = "mibs_2"
 
 # NO MODIFICAR
 datos = []
@@ -22,32 +22,31 @@ row = None
 rowb = None
 linea = ""
 
-parser = argparse.ArgumentParser(description=('compara dos archivo de '
-                                              'sumas de verificacion y '
-                                              'muestra los archivos '
-                                              'con diferencias.'))
-parser.add_argument('--resp', dest='file_b', action='store', default=None,
-                    type=str, help='Archivo de Respaldo', required=True)
-parser.add_argument('--prod', dest='file_a', action='store', default=None,
-                    type=str, help='Archivo de Productivo', required=True)
+parser = argparse.ArgumentParser(description=('Compare two SHASUM files and'
+                                              ' shows the diferences between'
+                                              ' them.'))
+parser.add_argument('--file2', dest='file_b', action='store', default=None,
+                    type=str, help='first SHASUM file', required=True)
+parser.add_argument('--file1', dest='file_a', action='store', default=None,
+                    type=str, help='Second SHASUM file', required=True)
 parser.add_argument('--out', dest='file_c', action='store',
-                    default='diferencias.txt', type=str,
-                    help='Archivo de salida', required=False)
+                    default='results.txt', type=str,
+                    help='Custom output filename', required=False)
 args = parser.parse_args()
 
 
 def procesar(args):
     bdata = [False, False, False]
     idstr = ""
-    print("Comenzando a procesar.")
+    print("Processing.")
 
     # Verificamos si el archivo A existe
     if not isfile(args.file_a):
-        print("No se encuentra el archivo ", args.file_a)
+        print("File not found.", args.file_a)
 
     # Verificamos si el archivo B existe
     if not isfile(args.file_b):
-        print("No se encuentra el archivo ", args.file_b)
+        print("File not found.", args.file_b)
 
     # Abrimos el archivo para leerlo
     with open(args.file_a, 'r') as archivoA:
@@ -56,7 +55,7 @@ def procesar(args):
         while linea != "":
             if linea == "":
                 break
-            row = linea.split(' ' + token_prod + '/')
+            row = linea.split(' ' + token1 + '/')
             datos.append([row[0], row[1], 1])
             linea = archivoA.readline()
 
@@ -71,28 +70,28 @@ def procesar(args):
             if linea == "":
                 break
             bdata = [False, False, False]
-            row = linea.split(' ' + token_resp + '/')
+            row = linea.split(' ' + token2 + '/')
             for rowb in datos:
                 if rowb[1] == row[1]:
                     bdata[0] = True
                     rowb[2] = 2
                     if rowb[0] != row[0]:
-                        idstr = "[DIFF]"
+                        idstr = "[=/=]"
             if bdata[0] is False:
-                idstr = "[SOBR]"
+                idstr = "[-1-]"
             if idstr != "":
                 diff.append([idstr, row[1]])
             idstr = ""
             linea = archivoB.readline()
         for rowb in datos:
             if rowb[2] != 2:
-                diff.append(["[FALT]", rowb[1]])
+                diff.append(["[-2-]", rowb[1]])
 
     with open(args.file_c, "w") as diffFile:
         for row in diff:
             diffFile.write(row[0] + " " + row[1])
 
-    print("Listo.")
+    print("Done.")
 
 
 procesar(args)
